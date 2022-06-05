f7fe39d1e57253471735bc06177406e51a3f67c1
omame-dev <edamame.dev@gmail.com>
2022-06-05 17:31:19 +0900

    Added Recoil, dark theme, and i18n support
    Wrapped App component as Recoil is not currently used but will be needed in the future.
    Dark theme currently uses useContext and createContext, but will be controlled by Recoil in the future. This is also in the form of wrapping the App component.
    i18n is located in the . /src/locales folder to support language changes by reading files defined in the /src/locales folder.

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from './components/theme';
+import './locales/i18n';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
   </React.StrictMode>
-)
+);
