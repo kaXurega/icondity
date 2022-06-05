d7e1c23788c2b1085c5aa1267ed8c5760a426a3a
omame-dev <edamame.dev@gmail.com>
2022-06-05 21:35:12 +0900

    Add vite-plugin-env-compatible
    process.env can now be used with vite.

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import env from 'vite-plugin-env-compatible';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    open: true
+  },
   plugins: [
     react(),
+    env({ prefix: "VITE", mountedPath: "process.env" })
   ],
   build: {
     outDir: "dist",
