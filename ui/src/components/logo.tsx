c6b98e4078cad759498f956af6599c5d11482fe8
omame-dev <edamame.dev@gmail.com>
2022-06-09 20:14:03 +0900

    Add Logo display area
    Added logo display area in logo.tsx. This component is a child component of Header.tsx.
    Currently, we do not have an image for the logo, so we use the h1 tag instead.

---
+++ ui/src/components/logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../layout/Header.scss';
+
+const logo = () => {
+  return (
+    <h1 className="logo">
+      icondity
+    </h1>
+  );
+}
+
+export default logo;
