fef8f0d96f597744ef6ae48c2277cbed0f6e39fd
omame-dev <edamame.dev@gmail.com>
2022-06-21 20:35:21 +0900

    Add scrollbar hide css
    Scrollbars are now hidden.

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { RecoilRoot } from 'recoil';
+// import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from './components/theme';
 import './locales/i18n';
-import './index.css';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RecoilRoot>
+  // <React.StrictMode>
+    // <RecoilRoot>
       <ThemeProvider>
         <App />
       </ThemeProvider>
-    </RecoilRoot>
-  </React.StrictMode>
+    // </RecoilRoot>
+  // </React.StrictMode>
 );
